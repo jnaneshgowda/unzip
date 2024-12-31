2f202a25d416acb56233db9ad810dd2074cea265
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-12-31 21:23:53 +0600

    Update config.py

--- Unzip/config.py
+++ Unzip/config.py
@@ -1,9 +1,12 @@
 import os
 
 class Config(object):
-     
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ.get("API_ID", ))
+    API_ID = int(os.environ.get("API_ID", ""))
     API_HASH = os.environ.get("API_HASH", "")
-    
-    
+
+    # Force Subscription Channel IDs
+    FORCE_SUB_CHANNELS = [
+        -1001764441595,  # Replace with your first channel ID
+        -1002135593873,  # Replace with your second channel ID
+    ]

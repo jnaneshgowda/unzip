d6f10c47ceccd169e746d3af53b5537220f12e3b
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2025-01-01 00:22:00 +0600

    Update config.py

--- Unzip/config.py
+++ Unzip/config.py
@@ -1,12 +1,18 @@
 import os
-import re 
+import re
 from os import environ
 
-id_pattern = re.compile(r'^.\d+$')
+id_pattern = re.compile(r'^-?\d+$')
+
+# Define AUTH_CHANNEL at the module level
+AUTH_CHANNEL = [
+    int(ch) if id_pattern.search(ch) else ch
+    for ch in environ.get('AUTH_CHANNEL', '-1001764441595 -1002135593873').split()
+]
 
 class config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ.get("API_ID", ""))
+    API_ID = int(os.environ.get("API_ID", "0"))
     API_HASH = os.environ.get("API_HASH", "")
-    AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '-1001764441595 -1002135593873').split()] # give channel id with seperate space. Ex : ('-10073828 -102782829 -1007282828')
+
     

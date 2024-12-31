8bf2aa7a92f8de9db0bfcac17851b3583ba936e9
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-12-31 21:46:49 +0600

    Update config.py

--- Unzip/config.py
+++ Unzip/config.py
@@ -1,12 +1,11 @@
 import os
+from os import environ
+
+id_pattern = re.compile(r'^.\d+$')
 
 class config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     API_ID = int(os.environ.get("API_ID", ""))
     API_HASH = os.environ.get("API_HASH", "")
-
-    # Force Subscription Channel IDs
-    FORCE_SUB_CHANNELS = [
-        -1001764441595,  # Replace with your first channel ID
-        -1002135593873,  # Replace with your second channel ID
-    ]
+    AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '-1001764441595 -1002135593873').split()] # give channel id with seperate space. Ex : ('-10073828 -102782829 -1007282828')
+    

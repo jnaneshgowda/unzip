1ff302a78860122ef93c21a7bbc97102e078c473
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-12-31 21:36:42 +0600

    Update commands.py

--- Unzip/commands.py
+++ Unzip/commands.py
@@ -3,7 +3,7 @@
 
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
-from config import config
+from Unzip.config import config
 
 active_tasks = {}
 
